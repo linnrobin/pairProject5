71000a3ed3a62b04cedeab53fbdda133a05fa58c
Ottoyd <ugaipribadi@gmail.com>
2020-03-26 15:51:55 +0700

    Merge Email& whatsApp

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # pairProject5
+
+NPM
+- npm install nodemailer
+- npm install twilio
+
+Add local Enviroment Variable
+- TWILIO_ACCOUNT_SID = Your SID key
--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+const accountSid = 'AC6249830ad7debac982793d7b2f249bed'
+const authToken = '6d0d1d13296e28c5c55f6e3023af9e19'
+const client = require('twilio')(accountSid, authToken);
 const nodemailer= require('nodemailer')
 
 let pelapor = {}
-
+// data dari database >> for each
 pelapor.name = 'Ottoyd'
 pelapor.email = 'ugaipribadi@gmail.com'
 pelapor.item = 'https://imgur.com/a/MuMV05p'
+pelapor.telp = '+6281385902476'
 
 let item = {}
 
@@ -33,3 +37,9 @@ trans.sendMail(option, (err,res) => {
         console.log('Email Sucessfuly send', res.response)
     }
 })
+
+client.messages.create({
+    from:'whatsapp:+14155238886',
+    to:`whatsapp:${pelapor.telp}`,
+    body:`Hello ${pelapor.name} barang anda ${item.name} ${pelapor.item} telah ditemukan & dapat di ambil di ${item.lokasi}`
+}).then(messages => console.log(messages.sid))
